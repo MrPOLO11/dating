144e75c18ba7d5a4e3da0b77f2c1b1b159795c06
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-17 16:09:06 -0500

    Added json file, stylessheet, and created an index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Marcos Rivera
+ * 01-15-2020
+ * 328/chicken/index.php
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once ("vendor/autoload.php");
+
+//Instantiate Fat-Free framework (F3)
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo"Hello, World";
+});
+
+$f3->run();

f941fe09095133b92c078beb2b177ce3d6d50685
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-02-09 23:58:36 -0500

    Completed Validation for personal page, which checks if first name, last name, age, and phone number are valid. Still need to find a work around dispalying only one or the other error for first and last name.

--- index.php
+++ index.php
@@ -11,10 +11,16 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //Require autoload file
-require_once ("vendor/autoload.php");
+require_once ('vendor/autoload.php');
+require_once ('model/validate.php');
 
 //Instantiate Fat-Free framework (F3)
 $f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define arrays
 
 //Define a default route
 $f3->route('GET /', function() {
@@ -23,18 +29,37 @@ $f3->route('GET /', function() {
 });
 
 //Define a personal route
-$f3->route('POST /personal', function() {
+$f3->route('GET|POST /personal', function($f3) {
     $view = new Template();
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $age = $_POST['age'];
+        $phone = $_POST['phone'];
+
+        $f3->set('fname', $fname);
+        $f3->set('lname', $lname);
+        $f3->set('age', $age);
+        $f3->set('phone', $phone);
+
+        if(validPersonalInformation()) {
+            $_SESSION['fname'] = $fname;
+            $_SESSION['lname'] = $lname;
+            $_SESSION['age'] = $age;
+            $_SESSION['phone'] = $phone;
+            $f3->reroute('/profile');
+        }
+    }
     echo $view->render('views/personal-information.html');
 });
 
 //Define a profile route
-$f3->route('POST /profile', function() {
-    $_SESSION['fname'] = $_POST['fname'];
+$f3->route('GET|POST /profile', function() {
+    /*$_SESSION['fname'] = $_POST['fname'];
     $_SESSION['lname'] = $_POST['lname'];
     $_SESSION['age'] = $_POST['age'];
     $_SESSION['gender'] = $_POST['gender'];
-    $_SESSION['telephone'] = $_POST['telephone'];
+    $_SESSION['telephone'] = $_POST['telephone'];*/
     $view = new Template();
     echo $view->render('views/profile.html');
 });
